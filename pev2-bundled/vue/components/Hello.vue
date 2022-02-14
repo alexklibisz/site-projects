143b7733d3c77af260ba50341980a0cd9aa76c6d
Alex Klibisz <aklibisz@gmail.com>
2022-02-13 23:17:03 -0500

    Reproduced pev2 codesandbox example

--- pev2-bundled/vue/components/Hello.vue
+++ pev2-bundled/vue/components/Hello.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>Hello, {{ name }}.</div>
+  <pev2 :plan-source="plan" :plan-query="query"></pev2>
 </template>
 
 <script>
+import pev2 from "pev2";
+
+const plan = `
+                                                           QUERY PLAN
+---------------------------------------------------------------------------------------------------------------------------------
+ Nested Loop  (cost=4.33..118.25 rows=10 width=488) (actual time=0.370..1.126 rows=10 loops=1)
+   ->  Bitmap Heap Scan on tenk1 t1  (cost=4.33..39.44 rows=10 width=244) (actual time=0.254..0.380 rows=10 loops=1)
+         Recheck Cond: (unique1 < 10)
+         ->  Bitmap Index Scan on tenk1_unique1  (cost=0.00..4.33 rows=10 width=0) (actual time=0.164..0.164 rows=10 loops=1)
+               Index Cond: (unique1 < 10)
+   ->  Index Scan using tenk2_unique2 on tenk2 t2  (cost=0.00..7.87 rows=1 width=244) (actual time=0.041..0.048 rows=1 loops=10)
+         Index Cond: (unique2 = t1.unique2)
+ Total runtime: 2.414 ms`;
+
+const query = `
+SELECT *
+FROM tenk1 t1, tenk2 t2
+WHERE t1.unique1 < 10 AND t1.unique2 = t2.unique2;`;
+
 export default {
   data() {
     return {
-      name: "World",
+      plan: plan,
+      query: query
     };
   },
+  components: {
+    pev2
+  }
 };
 </script>
 
--- pev2-bundled/vue/loaders/helloLoader.js
+++ pev2-bundled/vue/loaders/helloLoader.js
@@ -1,10 +1,8 @@
 import Vue from "vue";
 import Hello from "../components/Hello.vue";
 
-// The following creates a new instance of the component and puts it anywhere
-// a element has a data-vue-component="hello" attribute.
 document
-  .querySelectorAll("[data-vue-component=hello]")
+  .querySelectorAll("[data-vue-component=pev2]")
   .forEach((element) => {
     new Vue(Hello).$mount(element);
   });
