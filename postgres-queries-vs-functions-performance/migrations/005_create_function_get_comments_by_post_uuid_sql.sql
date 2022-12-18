73db71e10f1cf1d69ff98cd574133f4a239032d7
Alex Klibisz <aklibisz@gmail.com>
2022-12-18 10:28:35 -0600

    consolidate to one benchmark.py and switch to counting comments

--- postgres-queries-vs-functions-performance/migrations/005_create_function_get_comments_by_post_uuid_sql.sql
+++ postgres-queries-vs-functions-performance/migrations/005_create_function_get_comments_by_post_uuid_sql.sql
@@ -1,14 +1,14 @@
 -- +goose Up
-create function get_comments_by_post_uuid_sql(uuid)
-returns table(comment_uuid uuid, contents text, created_at timestamp, updated_at timestamp)
+create function comment_count_by_post_uuid(uuid)
+returns integer
 language sql
 parallel safe
 returns null on null input
 as $$
-    select comment_uuid, c.contents, c.created_at, c.updated_at
+    select count(comment_id)
     from comment c
     join post p on c.post_id = p.post_id
     where p.post_uuid = $1
 $$;
 -- +goose Down
-drop function get_comments_by_post_uuid_sql;
+drop function comment_count_by_post_uuid;
